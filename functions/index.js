dd90324771dcf402f1a6534cdfc4f399f3a7842e
thabangisaac1 <CSE17-008@thuto.bac.ac.bw>
2021-04-14 15:16:46 +0200

    register endpoint

--- functions/index.js
+++ functions/index.js
@@ -36,50 +36,31 @@ app.post('/api/register', (req, res) => {
 
     const {email, fullName, country} = req.body;
 
-    // auth.createUser(
-    //        {
-    //           email: email,
-    //           emailVerified: false,
-    //           password: "123456",
-    //           country: country,
-    //           displayName: fullName,
-    //           disabled: false,
-    //           customClaims: { roleId: 1 }
-    //          }
+    auth.createUser(
+           {
+              email: email,
+              emailVerified: false,
+              password: "123456",
+              country: country,
+              displayName: fullName,
+              disabled: false,
+              customClaims: { roleId: 1 }
+             }
      
-    //     ).then(() => {
-    //        console.log("success");      
-    //     }).catch(error=>{
-    //         console.log(error)
-    //     });
+        ).then(() => {
+          console.log("success");  
 
-    var actionCodeSettings = {
-      url:
-        ' https://bamb-leads.firebaseapp.com/user/reset-password?mode=<action>&oobCode=<code>', //enter the url of homepage which you want to sent to user's email.
-      handleCodeInApp: true,
-    };
+          // Email send to:
 
-    auth
-      .sendPasswordResetEmail(email, actionCodeSettings)
+          return res.status(202).json("email sent!")          
 
-      .then(function () {
-        // The link was successfully sent. Inform the user.
-        // Save the email locally so you don't need to ask the user for it again
-        // if they open the link on the same device.
-        window.localStorage.setItem('emailForSignIn', email);
-        NotificationManager.primary(
-          'Email Succesfully Sent.',
-          'Email Confirmation To User',
-          3000,
-          null,
-          null
-        );
-      })
-      .catch(function (error) {
-        console.log(error);
-        alert('failed....');
-        // Some error occurred, you can inspect the code: error.code
-      });
+        }).catch(error=>{
+          console.log(error)
+          return res.status(500).json(error)  
+            
+        });
+
+   
     
 
 
