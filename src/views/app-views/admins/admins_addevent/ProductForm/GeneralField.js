bf884d69d8822d0174d1a81f30868efc1029bd6b
Pro Chivs <prochivia@gmail.com>
2021-05-11 20:51:40 +0200

    Events Form

--- src/views/app-views/admins/admins_addevent/ProductForm/GeneralField.js
+++ src/views/app-views/admins/admins_addevent/ProductForm/GeneralField.js
@@ -122,7 +122,7 @@ const GeneralField = (props) => {
         <Row gutter={16}>
 
           <Col xs={24} sm={24} md={12}>
-            <Form.Item name="sporting_code" label="Sporting Code Name" rules={rules.code}>
+            <Form.Item name="sporting_code" label="Sporting Code Name" rules={rules.codeName}>
               <Input  name="codeName" className="w-100"     value={data.codeName} placeholder="Example - Tennis"
                 onChange={handle}/>
             </Form.Item>
@@ -134,13 +134,13 @@ const GeneralField = (props) => {
             </Form.Item>
           </Col>
           <Col xs={24} sm={24} md={12}>
-            <Form.Item name="countries_participating" label="Countries Participating" rules={rules.countries}>
+            <Form.Item name="countries_participating" label="Countries Participating" rules={rules.countriesParticipating}>
               <Input  name="countriesParticipating" value={data.countriesParticipating}
                 onChange={handle}  placeholder="Example - Botswana, Zimbabwe, Malawi"/>
             </Form.Item>
           </Col>
           <Col xs={24} sm={24} md={12}>
-            <Form.Item  label="Date Scheduled" rules={rules.date}>
+            <Form.Item  label="Date Scheduled" rules={rules.dateScheduled}>
             <Input
                             type="date"
                             name="dateScheduled"
@@ -152,20 +152,20 @@ const GeneralField = (props) => {
           </Col>
 
           <Col xs={24} sm={24} md={12}>
-            <Form.Item name="location" label="Event Location" rules={rules.locations}>
+            <Form.Item name="location" label="Event Location" rules={rules.eventLocation}>
               <Input  name="eventLocation" value={data.eventLocation}
                 onChange={handle}  placeholder="Example - Great Mansion Grounds"/>
             </Form.Item>
           </Col>
           <Col xs={24} sm={24} md={12}>
-            <Form.Item name="link" label="Virtual Link" rules={rules.virtual}>
+            <Form.Item name="link" label="Virtual Link" rules={rules.virtualLink}>
               <Input  name="virtulLink" value={data.virtualtLink}
                 onChange={handle}  placeholder="Livestream - Google Link"/>
             </Form.Item>
           </Col>
 
           <Col xs={24} sm={24} md={24}>
-            <Form.Item name="description" label="Event Description" rules={rules.event_description}>
+            <Form.Item name="description" label="Event Description" rules={rules.eventDescription}>
               <Input.TextArea  name="eventDescription" value={data.eventDescription}
                 onChange={handle}  placeholder="Short brief description"/>
             </Form.Item>
